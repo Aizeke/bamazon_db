483512788b2334f1aac5b1edf07dfadc1f063009
Isaac Guido <43936703+Aizeke@users.noreply.github.com>
2019-08-08 15:35:08 -0700

    Completed Challenge #1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '%I_Met_The_Devil_In_Miami%',
     database: 'bamazon_db'
 });
 
@@ -20,37 +20,47 @@ var showData = function () {
         if (err) throw err;
 
         for (var i = 0; i < data.length; i++) {
-            console.log("ID: " + data[i].item_id + " Product: " + data[i].product_name + " Price: " + data[i].price + '\n');
+            console.log("ID: " + data[i].item_id + '\n' + "Product: " + data[i].product_name + "\nPrice: " + data[i].price + '\n');
         }
+
+        purchasePrompt();
     });
 };
 
 var purchasePrompt = function() {
     inquirer.prompt([
         {
-            type:'number',
+            type:'input',
             message:"What is the ID of the item you are try to purchase?",
-            name: 'productId',
-            validate: function(input) {
-                if(typeof input !== 'number'){
-                    console.log("You need to provide a Number!!");
-                    return;
-                }
-            }
+            name: 'productId'
         },
         {
-            type: 'number',
+            type: 'input',
             message: "How many units would you like to purchase?",
-            name: 'productQty',
-            validate: function(input) {
-                if(typeof input !== 'number'){
-                    console.log("You need to provide a Number!!");
-                    return;
-                }
-            }
+            name: 'productQty'
         }
     ])
     .then(function(res){
-        console.log(res);
+
+        var id = parseInt(res.productId);
+        var qty = parseInt(res.productQty);
+
+        connection.query('SELECT stock_quantity, price FROM products WHERE item_id=' + id, function(err, response){
+            if(err) throw err;
+
+            var stock = response[0].stock_quantity;
+
+            if(qty > stock){
+                console.log("There is an insufficient amount of stock in our warehouse!");
+                purchasePrompt();
+            } else{
+                connection.query(`UPDATE products SET stock_quantity = ${stock-=qty} WHERE item_id=${id}`, function(err){
+                    if(err) throw err;
+                    console.log("Purchase went through\nTotal: $" + response[0].price);
+                    connection.end();
+                })
+            }
+        });
+
     });
 }
